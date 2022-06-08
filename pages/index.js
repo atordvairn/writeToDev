d0ead0c821ba8c500b2682e3975e95827ba26670
atordvairn <atordvairn@gmail.com>
2022-06-08 10:46:16 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
 export default function Index() {
   return (
-    <p>
-      To test the CORS route, open the console in a new tab on a different
-      domain and make a POST / GET / OPTIONS request to <b>/api/cors</b>. Using
-      a different method from those mentioned will be blocked by CORS
-    </p>
+<>
+    <h1>writeToDev</h1>
+
+<i>only posts requests to the api will be processed</i>
+
+endpoint:
+<code>
+https://write-to-dev.vercel.app/api/publish
+</code>
+<p>
+post a request as;
+<code>
+{
+    "title": "test",
+    "body": "this is markdown for the body",
+    "api_key": "<YOU API KEY FROM DEV.TO>",
+    "tags": ["webdev", "JavaScript", "test", "only4"]
+}
+</code>
+</p>
+</>
   );
 }
