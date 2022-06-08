2acda6a82685a323b6d2023d3a41b35670c53425
atordvairn <atordvairn@gmail.com>
2022-06-08 05:33:48 +0000

    front page

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
 export default function Index() {
   return (
-<>
-    <h1>writeToDev</h1>
-
-<i>only posts requests to the api will be processed</i>
-
-endpoint:
-<code>
-https://write-to-dev.vercel.app/api/publish
-</code>
-<p>
-post a request as;
-<code>
-{
-    "title": "test",
-    "body": "this is markdown for the body",
-    "api_key": "<YOU API KEY FROM DEV.TO>",
-    "tags": ["webdev", "JavaScript", "test", "only4"]
-}
-</code>
-</p>
-</>
+    <>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+      <h1 class="display-1 m-3">writeToDev</h1>
+      <i class="m-3 text-muted">only posts requests to the api will be processed</i>
+      <br/>
+      <code class="m-3">
+         endpoint : https://write-to-dev.vercel.app/api/publish
+      </code>
+      <p class="mt-4 m-3">
+        post a request as;
+        <br />
+        <code>
+          {`{`}<br />
+          <span class="m-3">"title": "test",</span><br />
+          <span class="m-3">"body": "this is markdown for the body",</span><br />
+          <span class="m-3">"api_key": {`"<YOU API KEY FROM DEV.TO>"`},</span><br />
+          <span class="m-3">"tags": {`["webdev", "JavaScript", "test", "only4"]`}</span><br />
+          {`}`}
+        </code>
+      </p>
+    </>
   );
 }
